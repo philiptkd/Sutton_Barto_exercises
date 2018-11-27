e93ae0ed9f66fa575c98e64532a45457ae60cbfc
philiptkd <philiptkd1@gmail.com>
2018-11-26 21:47:35 -0600

    finished Fig. 5.2, though slightly different

--- Monte Carlo Methods/blackjack_ctrl.py
+++ Monte Carlo Methods/blackjack_ctrl.py
@@ -7,9 +7,10 @@ import gym
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import pickle
 
 env = gym.make('Blackjack-v0')
-episodes = 500000
+episodes = 3000000
 num_actions = env.action_space.n
 
 def obs_to_state(observation):
@@ -39,7 +40,8 @@ for episode in range(episodes):
         if observation[0] >= 12:
             i,j,k = obs_to_state(observation)
             state = (i, j, k, action)
-            visited_states.append(state)
+            if state not in visited_states: # trying first visit
+                visited_states.append(state)
 
         # get output
         observation, reward, done, info = env.step(action)
@@ -60,21 +62,51 @@ for episode in range(episodes):
     # update policy
     policy = np.argmax(values[:,:,:,:,0], axis=-1)
 
-# plot values
-X = Y = range(10)
-X, Y = np.meshgrid(X, Y)
-values = np.max(values[:,:,:,:,0], axis=-1)
-Z1 = values[:,:,0]
-Z2 = values[:,:,1]
+## plot values
+#X = Y = range(10)
+#X, Y = np.meshgrid(X, Y)
+#values = np.max(values[:,:,:,:,0], axis=-1)
+#Z1 = values[:,:,0]
+#Z2 = values[:,:,1]
+#
+#fig = plt.figure()
+#ax = fig.add_subplot(211, projection='3d')
+#ax.plot_wireframe(X,Y,Z1)
+#ax.set_title("No usable ace")
+#
+#ax2 = fig.add_subplot(212, projection='3d')
+#ax2.plot_wireframe(X,Y,Z2)
+#ax2.set_title("Usable ace")
+#
+#plt.show()
+#
+## save to plot later
+#with open("policy_file", "wb+") as f:
+#    pickle.dump(policy, f)
+
+# plot policy
+plt.clf()
+# get x values for step plot
+x = range(10)
+x = np.array(list(zip(x,x))).reshape(-1)
+x[:-1] = x[1:]
+x[-1] = 10
 
 fig = plt.figure()
-ax = fig.add_subplot(211, projection='3d')
-ax.plot_wireframe(X,Y,Z1)
+ax = fig.add_subplot(211)
+y1 = np.sum(policy[:,:,0], axis=0)
+y1 = np.array(list(zip(y1,y1))).reshape(-1)
+y1 += 11
+ax.plot(x, y1)
+ax.set_ylim([10,20])
 ax.set_title("No usable ace")
 
-ax2 = fig.add_subplot(212, projection='3d')
-ax2.plot_wireframe(X,Y,Z2)
+ax2 = fig.add_subplot(212)
+y2 = np.sum(policy[:,:,1], axis=0)
+y2 = np.array(list(zip(y2,y2))).reshape(-1)
+y2 += 11
+ax2.plot(x, y2)
+ax2.set_ylim([10,20])
 ax2.set_title("Usable ace")
 
 plt.show()
-
